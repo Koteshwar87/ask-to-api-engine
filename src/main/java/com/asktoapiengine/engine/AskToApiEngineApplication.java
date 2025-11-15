f3ac426ccb2bc60d1c744038c5bc1a309fc404bd
unknown <kotesh-full-access>
2025-11-15 13:42:59 +0530

    Initial commit

---
+++ src/main/java/com/asktoapiengine/engine/AskToApiEngineApplication.java
@@ -0,0 +1,13 @@
+package com.asktoapiengine.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskToApiEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskToApiEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asktoapiengine/engine/AskToApiEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asktoapiengine.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskToApiEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
